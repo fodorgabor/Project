85f117d707de4e093a09b18196e0fb5a324ce4bb
fodorgabor <127303784+fodorgabor@users.noreply.github.com>
2026-04-10 20:34:11 +0200

    Add files via upload

---
+++ Sutinek-main/main.js
@@ -0,0 +1,4 @@
+// const STEAMID = 76561199026570558;
+// fetch("https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=APIKULCS&steamid=" + STEAMID + "&include_appinfo=true")
+// .then(res => res.json())
+// .then(data => console.log(data))
