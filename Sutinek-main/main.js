6592fbcedd0eeff754e2568be5097e3f7b71c124
fodorgabor <127303784+fodorgabor@users.noreply.github.com>
2026-04-18 20:49:10 +0200

    Add files via upload

--- Sutinek-main/main.js
+++ Sutinek-main/main.js
@@ -75,7 +75,7 @@ function displayPage(page) {
         gameDiv.className = 'game';
 
         gameDiv.innerHTML = `
-            <img src="https://cdn.cloudflare.steamstatic.com/steam/apps/${game.appid}/header.jpg" alt="${game.appid}">
+            <img src="${game.image || `https://cdn.cloudflare.steamstatic.com/steam/apps/${game.appid}/header.jpg`}" alt="${game.name}">
             <div class="title">${game.name}</div>
             <a href="https://store.steampowered.com/app/${game.appid}/" target="_blank" class="btn">
                 Steam oldal megtekintése
